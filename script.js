39b97743b9787f9df79890ea80d7b3943ab7c8b1
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-04 10:08:30 -0300

    Tudo pronto e funcionando
    Elaborando um novo design

--- script.js
+++ script.js
@@ -6,24 +6,47 @@ const BottomLeft = document.querySelector('#BottomLeft')
 const box = document.querySelector('.box')
 const showBorder = document.querySelector('.showBorder')
 
-TopLeft.addEventListener('input', e => box.style. borderTopLeftRadius = e.target.value + 'px',  
-)
+TopLeft.addEventListener('input', function(e){ 
+    box.style.borderTopLeftRadius = e.target.value + 'px';
+    TopLeft.value = e.target.value
+    showCSS()
+})
 
+TopRight.addEventListener('input', function(e){
+    box.style.borderTopRightRadius = e.target.value + 'px'
+    TopRight.value = e.target.value
+    showCSS()
+})
 
-TopRight.addEventListener('input', e => box.style. borderTopRightRadius = e.target.value + 'px'  
-)
+BottomRight.addEventListener('input', function(e){
+    box.style. borderBottomRightRadius = e.target.value + 'px'
+    BottomRight.value = e.target.value
+    showCSS()
+})
 
-BottomRight.addEventListener('input', e => box.style. borderBottomRightRadius = e.target.value + 'px'  
-)
-
-BottomLeft.addEventListener('input', e => box.style. borderBottomLeftRadius = e.target.value + 'px'  
-)
+BottomLeft.addEventListener('input', function(e){
+    box.style. borderBottomLeftRadius = e.target.value + 'px',
+    BottomLeft.value = e.target.value, 
+    showCSS()
+})
 
 function showCSS(){
-    showBorder.innerHTML = `border: ${TopLeft.value} ${TopRight} ${BottomRight} ${BottomLeft};`
+    const showBorderInput = document.querySelector('#showBorderInput')
+    showBorderInput.innerHTML = `border: ${TopLeft.value} ${TopRight.value} ${BottomRight.value} ${BottomLeft.value};`
 }
-console.log(TopLeft.value)
-// botao de copiar
+
+function copy(){
+    
+    showBorderInput.select()
+    showBorderInput.setSelectionRange(0, 99999)
+
+    document.execCommand("copy")
+
+    alert('Texto copiado')
+}
+const button = document.querySelector('button')
+button.addEventListener('click', copy)
+
 
 
 
