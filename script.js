dcb12a3ad1fa60b87f20d8eb5c8554b9964212b6
Luis Gustavo Macedo <luisgustavomacedo13@gmail.com>
2021-06-03 23:55:26 -0300

    Código um pouco mais rabuscado
    Todas as bordas já funcionando

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-
-const inputs = document.querySelector('input')
 const TopLeft = document.querySelector('#TopLeft')
-const $TopRight = document.querySelector('#TopRight')
-const $BottomRight = document.querySelector('#BottomRight')
-const $BottomLeft = document.querySelector('#BottomLeft')
+const TopRight = document.querySelector('#TopRight')
+const BottomRight = document.querySelector('#BottomRight')
+const BottomLeft = document.querySelector('#BottomLeft')
 
-const $box = document.querySelector('.box')
+const box = document.querySelector('.box')
+const showBorder = document.querySelector('.showBorder')
 
-function makeBorder(TopLeft){
-        $box.style.borderTopLeftRadius = TopLeft.target.value + 'px';
-        console.log(TopLeft)
-    }
+TopLeft.addEventListener('input', e => box.style. borderTopLeftRadius = e.target.value + 'px',  
+)
 
-inputs.oninput = makeBorder
 
-// mostrar o css pronto
+TopRight.addEventListener('input', e => box.style. borderTopRightRadius = e.target.value + 'px'  
+)
+
+BottomRight.addEventListener('input', e => box.style. borderBottomRightRadius = e.target.value + 'px'  
+)
+
+BottomLeft.addEventListener('input', e => box.style. borderBottomLeftRadius = e.target.value + 'px'  
+)
+
+function showCSS(){
+    showBorder.innerHTML = `border: ${TopLeft.value} ${TopRight} ${BottomRight} ${BottomLeft};`
+}
+console.log(TopLeft.value)
 // botao de copiar
+
+
+
+
